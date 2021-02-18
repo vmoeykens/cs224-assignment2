f421f6abe20f194a8434010de90af66d536cfed4
vmoeykens <vincent@beta.team>
2021-02-17 22:33:14 -0500

    Adds human input for start vertex

--- Graph.java
+++ Graph.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public final class Graph {
+    // Hardcoding this for now... this could be much better (for example reading from a CSV file and building it)
     public ArrayList<LinkedList<Integer>> graph = new ArrayList<LinkedList<Integer>>(Arrays.asList(new LinkedList<Integer>(Arrays.asList(2, 3)),
                                                                                                           new LinkedList<Integer>(Arrays.asList(1, 3, 4, 5)),
                                                                                                           new LinkedList<Integer>(Arrays.asList(1, 2, 5, 7, 8)),
--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Main { 
   
     public static void main(String[] args) 
     { 
+        Scanner in = new Scanner(System.in);
+
         Graph graph1 = new Graph();
         Graph graph2 = new Graph();
-
-        DepthFirstSearch.begin(graph1.graph, 1);
-        BreadthFirstSearch.begin(graph2.graph, 1);
+        
+        int maxVertexValue = graph1.graph.size();
+        int startVertex = -1;
+        while (startVertex < 1 || startVertex > maxVertexValue) {
+            System.out.println("Enter a number between 1 and " + maxVertexValue);
+            String startVertexString = in.nextLine();
+            startVertex = Integer.parseInt(startVertexString);
+        }
+        
+        DepthFirstSearch.begin(graph1.graph, startVertex);
+        BreadthFirstSearch.begin(graph2.graph, startVertex);
     } 
 } 
